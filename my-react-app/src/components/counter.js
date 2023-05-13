69997629316b5c535479774f7c89e2a9d859a28c
SildiKuti <58624800+SildiKuti@users.noreply.github.com>
2023-05-13 03:36:56 +0200

    14th ex

--- my-react-app/src/components/counter.js
+++ my-react-app/src/components/counter.js
@@ -1,26 +1,24 @@
-// Create a Counter class component with an internal state containing a count property, initialized to 0. 
-// The Counter component should render the count property within an h1 tag,
-//  and the count property should be incremented by 1 every second;
-
-import React, { useState, useEffect } from 'react';
-import CounterDisplay from './CounterDisplay';
-
-const Counter = (prop) => {
-    
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 1000);
-
-
-  return () => clearInterval(intervalId);
-}, []);
-
-return <CounterDisplay count={count} />;
-
-};
-
-export default Counter;
-
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = {
+      count: 0,
+    };
+  
+    componentDidMount() {
+      this.intervalId = setInterval(() => {
+        this.setState((prevState) => ({
+          count: prevState.count + 1,
+        }));
+      }, 1000);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.intervalId);
+    }
+  
+    render() {
+      return <h1>{this.state.count}</h1>;
+    }
+  }
+  
+  export default Counter;
