c317475442d1c5bd40e045be82bdac219a1050cd
SildiKuti <58624800+SildiKuti@users.noreply.github.com>
2023-05-14 18:26:06 +0200

    19thex

--- my-react-app/src/components/InteractiveComponent.js
+++ my-react-app/src/components/InteractiveComponent.js
@@ -16,20 +16,26 @@ export class InteractiveComponent extends React.Component{
         this.setState({ [name]: type === "checkbox" ? checked : value })
     }
 
-    handleData = () => {
-        this.props.handleData(this.state)
+    handleState = () => {
+        this.setState({
+            username: '',
+            password: '',
+            remember: '',
+        })
     }
 
     render() {
         return <>
-        <form className='login-form'>
+        <div>
+         <button onClick={this.handleState}><h2>Reset</h2></button>
+        </div>
+        <div>
          <input name='username' value={this.state.username} onChange={this.handleInput}></input>
          <input name='password' type='password' value={this.state.password} onChange={this.handleInput}></input>
          <input name='remember' type='checkbox' checked={this.state.remember} onChange={this.handleInput}></input>
-        </form>
-        <div>
-         <button type='submit' name='button' onClick={this.handleData} disabled={this.state.username === '' || this.state.password === '' ? true : false}><h2>Login</h2></button>
-        </div>
+         </div>
         </>
     }
 }
+
+export default InteractiveComponent;
