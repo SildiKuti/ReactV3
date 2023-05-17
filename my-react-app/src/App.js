8f5ff8b906b690f055b7d5ec5248beb04b3b4396
SildiKuti <58624800+SildiKuti@users.noreply.github.com>
2023-05-17 02:21:52 +0200

    custom git

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,20 +1,19 @@
 import "./App.css";
-import DisplayLanguage from "./components/DisplayLanguage";
-import InteractiveComponent from "./components/InteractiveComponent";
-import LanguageContext1 from "./components/LanguageContext";
-import ToDoList from "./components/ToDoList";
-import Counter from "./components/counter";
-import LanguageProvider from "./components/LanguageContext";
-import GitHub from "./components/github";
-import CustomCounter from "./components/customCounter";
-import GitHubUser from "./components/GitHubUser";
-
+// import DisplayLanguage from "./components/DisplayLanguage";
+// import InteractiveComponent from "./components/InteractiveComponent";
+// import LanguageContext1 from "./components/LanguageContext";
+// import ToDoList from "./components/ToDoList";
+// import Counter from "./components/counter";
+// import LanguageProvider from "./components/LanguageContext";
+// import GitHub from "./components/github";
+// import CustomCounter from "./components/customCounter";
+// import GitHubUser from "./components/GitHubUser";
+// import useGithubUser from "./components/UseGitHubUser";
 
 function App() {
   return <div>
-    <GitHub/>
-    <GitHubUser/>
-    <CustomCounter/>
+   
+    <useGithubUser/>
 
    
   </div>;
